f8222349ca01c6eb448b630735f11f7ce2e1fc5b
reganmeloche <reganmeloche@gmail.com>
2024-10-19 15:19:37 -0400

    cleanup

--- Services/MqttClient.cs
+++ Services/MqttClient.cs
@@ -30,23 +30,11 @@ namespace DogNode.Services
                     WithPassword(optValue.Password).
                     Build();
             _client = new HiveMQClient(mqttOptions);
-
-            // _client.OnMessageReceived += (sender, args) =>
-            // {
-            //     Console.WriteLine("Message Received: {}", args.PublishMessage.PayloadAsString);
-            // };
         }
 
         public async Task Send(string message) {
             // Connect to broker
             var connectResult = await _client.ConnectAsync().ConfigureAwait(false);
-
-            // Configure the subscriptions we want and subscribe
-            // var builder = new SubscribeOptionsBuilder();
-            // builder.WithSubscription("topic1", QualityOfService.AtLeastOnceDelivery)
-            //     .WithSubscription("topic2", QualityOfService.ExactlyOnceDelivery);
-            // var subscribeOptions = builder.Build();
-            // var subscribeResult = await _client.SubscribeAsync(subscribeOptions);
 
             // Publish a message
             var publishResult = await _client.PublishAsync("sample", message);
